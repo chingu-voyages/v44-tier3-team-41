6fe38047e4cda29ececb1edad3439b0904bbf7e5
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-05-04 23:32:53 -0400

    addition of page and component dirc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import HomePage from './page/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='flex justify-center'>
-      <div className='flex'>Hello World!</div>
+      <HomePage />
     </div>
   )
 }
