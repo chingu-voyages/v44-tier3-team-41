ef5ce3ddb5a200fc214f4562776905caaa88940c
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-05-04 21:47:03 -0400

    creation of client directory

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center'>
+
+      <div className='flex'>Hello World!</div>
+    </div>
+  )
+}
+
+export default App
