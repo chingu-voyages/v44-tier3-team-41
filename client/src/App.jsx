dc20e4ed984c682ea636fb924bcba440537fcb71
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-05-09 00:06:15 -0400

    formatting updates to app.jsx and main.jsx files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-import HomePage from './page/HomePage';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="flex justify-center">
-      <HomePage />
-    </div>
-  );
-=======
 function App() {
   return <div>app</div>;
->>>>>>> dev
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,53 +1,41 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './page/Home.jsx';
+import LoginPage from './page/Login.jsx';
+import SignupPage from './page/Signup.jsx';
+import MentorDetail from './page/MentorDetail.jsx';
+import MenteeDetail from './page/MenteeDetail.jsx';
 import App from './App.jsx';
 import './index.css';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./page/Home.jsx";
-import LoginPage from "./page/Login.jsx";
-import SignupPage from "./page/Signup.jsx";
-import MentorDetail from "./page/MentorDetail.jsx";
-import MenteeDetail from "./page/MenteeDetail.jsx";
-import App from "./App.jsx";
-import "./index.css";
->>>>>>> dev
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: <SignupPage />,
   },
   {
-    path: "/mentor_detail/1",
+    path: '/mentor_detail/1',
     // path: "/mentor_detail/:id",
     element: <MentorDetail />,
   },
   {
-    path: "/mentee_detail/1",
+    path: '/mentee_detail/1',
     // path: "/mentor_detail/:id",
     element: <MenteeDetail />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App />
-  </React.StrictMode>,
-=======
     <RouterProvider router={router} />
-  </React.StrictMode>
->>>>>>> dev
+  </React.StrictMode>,
 );
