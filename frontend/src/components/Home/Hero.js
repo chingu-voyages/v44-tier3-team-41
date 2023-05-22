0df6cde1be3e27fbc39830a826d474baa8c887b5
Daurius Swarts <ali@openmindi.co.za>
2023-05-22 15:37:12 +0200

    [BUG] replaced # with / in href.

--- frontend/src/components/Home/Hero.js
+++ frontend/src/components/Home/Hero.js
@@ -22,7 +22,7 @@ export default function Hero() {
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
-            <a className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">
                 DevelopMe
               </span>
@@ -90,7 +90,7 @@ export default function Hero() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a
-                href="#"
+                href="/"
                 className="-m-1.5 p-1.5"
               >
                 <span className="sr-only">
@@ -133,13 +133,13 @@ export default function Hero() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
                   </a>
                   <a
-                    href="#"
+                    href="/"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Sign up
