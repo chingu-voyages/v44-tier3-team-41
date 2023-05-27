93abd64e9c0ace2dc72c80ce3eab0ef027ffb447
Daurius Swarts <ali@openmindi.co.za>
2023-05-27 14:51:44 +0200

    [STYLE] Updated Login & Sign up pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			keyframes: {
+				float: {
+					'0%': {transform: 'translateY(0)'},
+					'50%': {transform: 'translateY(-15px)'},
+					'100%': {transform: 'translateY(0)'},
+				},
+			},
+			animation: {
+				float: 'float 3s ease-in-out infinite',
+			},
+		},
+	},
+	plugins: [],
+};
