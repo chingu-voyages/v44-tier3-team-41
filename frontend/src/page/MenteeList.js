2a6a5aefd50fe15ddf23049de6f7548c67fd5ec7
Daurius Swarts <ali@openmindi.co.za>
2023-05-28 13:25:02 +0200

    [STYLE] Card updates

--- frontend/src/page/MenteeList.js
+++ frontend/src/page/MenteeList.js
@@ -1,39 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import MenteeCard from '../components/MenteeCard/MenteeCard';
-import { useSelector, useDispatch } from 'react-redux';
-import { getAllMenteesThunk } from '../store/mentee'
+import {
+	useSelector,
+	useDispatch,
+} from 'react-redux';
+import {getAllMenteesThunk} from '../store/mentee';
+import {UserIcon} from '@heroicons/react/24/outline';
 
 export default function MenteeList() {
-
 	const [options, setOptions] = useState('all');
-	const dispatch = useDispatch()
-	const mentees = useSelector(state => state.mentee.search)
+	const dispatch = useDispatch();
+	const mentees = useSelector(
+		state => state.mentee.search
+	);
 
 	useEffect(() => {
 		const fetchData = async () => {
-			await dispatch(getAllMenteesThunk())
+			await dispatch(getAllMenteesThunk());
 		};
-		fetchData()
+		fetchData();
 	}, [dispatch]);
-
 
 	return (
 		<div>
-			<div className="relative">
-				<div
-					className="absolute inset-0 flex items-center"
-					aria-hidden="true">
-					<div className="w-full border-t border-gray-300" />
-				</div>
+			<div className="mb-4 z-10 bg-gradient-to-r from-green-700 to-green-950 shadow-lg shadow-green-700/20 w-1/4 rounded-lg">
+				<h2 className="p-3 text-center text-green-100 font-normal text-sm tracking-wide">
+					<UserIcon className="w-4 h-4 inline-block align-text-bottom mr-1" />
+					Mentee list
+				</h2>
 			</div>
-			<div>
-				<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+			<div className="z-0">
+				<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-[#fafafa] p-4 py-10 rounded-lg shadow-lg ">
 					{mentees?.map((mentee, index) => (
-						<MenteeCard key={index} mentee={mentee} />
+						<MenteeCard
+							key={index}
+							mentee={mentee}
+						/>
 					))}
 				</ul>
 			</div>
 		</div>
 	);
-
 }
--- frontend/src/page/MentorList.js
+++ frontend/src/page/MentorList.js
@@ -6,6 +6,7 @@ import {
 	useDispatch,
 } from 'react-redux';
 import {getAllMentorsThunk} from '../store/mentor';
+import {UserIcon} from '@heroicons/react/24/outline';
 
 export default function MentorList() {
 	const dispatch = useDispatch();
@@ -22,7 +23,13 @@ export default function MentorList() {
 
 	return (
 		<div>
-			<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-[#fafafa] p-4 rounded-lg shadow-lg">
+			<div className="mb-4 z-10 bg-gradient-to-r from-indigo-700 to-indigo-950 shadow-lg shadow-indigo-700/30 w-1/4 rounded-lg">
+				<h2 className="p-3 text-center text-indigo-100 font-normal text-sm tracking-wide">
+					<UserIcon className="w-4 h-4 inline-block align-text-bottom mr-1" />
+					Mentor list
+				</h2>
+			</div>
+			<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-[#fafafa] p-4 py-10 rounded-lg shadow-lg">
 				{mentors?.map((mentor, index) => (
 					<MentorCard
 						key={index}
