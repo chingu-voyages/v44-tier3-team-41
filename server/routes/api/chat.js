907f207bed2b2361990cc5fb1f5654bf5d21ba51
Alexander Hunt <alexhunt215@gmail.com>
2023-06-01 13:56:16 -0400

    updated the location of the openai API Key

--- server/routes/api/chat.js
+++ server/routes/api/chat.js
@@ -1,25 +1,28 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { OpenAIApi } = require('openai');
 const express = require('express');
+const { OPENAI_API_KEY } = require('../../config');
 
-const configuration = new Configuration({
-    organization: 'org-RXoakR4d3wdHI1q8QoZIq3Xy',
-    apiKey: '********************',
-});
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(OPENAI_API_KEY);
 
 const router = express.Router();
 
-//! Openai API
+//! OpenAI API
 router.post('/', async (req, res) => {
     const { input } = req.body;
 
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: input,
-        max_tokens: 100,
-        temperature: 0.5,
-    });
-    res.json({ message: response.data.choices[0].text });
+    try {
+        const response = await openai.createCompletion({
+            engine: 'davinci',
+            prompt: input,
+            max_tokens: 100,
+            temperature: 0.5,
+        });
+
+        res.json({ message: response.choices[0].text });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
 });
 
 module.exports = router;
