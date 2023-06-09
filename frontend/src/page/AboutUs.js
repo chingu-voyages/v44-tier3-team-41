fbac40b8c1e69fb40986d9e5f76febdb35caecb5
Daurius Swarts <ali@openmindi.co.za>
2023-06-09 17:46:57 +0200

    [STYLE] Removed Contact form

--- frontend/src/page/AboutUs.js
+++ frontend/src/page/AboutUs.js
@@ -1,13 +1,17 @@
-import { Player } from '@lottiefiles/react-lottie-player';
+import {Player} from '@lottiefiles/react-lottie-player';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 
 const AboutUs = () => {
-	const navigate = useNavigate()
-	const sessionUser = useSelector(state => state.session.user)
+	const navigate = useNavigate();
+	const sessionUser = useSelector(
+		state => state.session.user
+	);
 
-	if (!sessionUser) { return navigate('/login') }
+	if (!sessionUser) {
+		return navigate('/login');
+	}
 	return (
 		<div>
 			<section className="bg-light1 text-dark1 rounded-xl shadow-lg">
@@ -256,82 +260,22 @@ const AboutUs = () => {
 			</div>
 
 			{/* Contact */}
-			<div className="container my-24 mx-auto md:px-6">
+			<div className="container my-10 mx-auto md:px-6">
 				<section className="mb-32">
 					<div className="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://mdbcdn.b-cdn.net/img/new/textures/full/284.jpg')]"></div>
 					<div className="container px-6 md:px-12">
 						<div className="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
 							<div className="flex flex-wrap">
-								<div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
-									<form>
-										<div
-											className="relative mb-6 border border-light1 rounded-lg"
-											data-te-input-wrapper-init>
-											<input
-												type="text"
-												className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none placeholder:text-dark3 peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-												id="exampleInput90"
-												placeholder="Name"
-											/>
-											<label
-												className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  peer-focus:text-primary"
-												for="exampleInput90">
-												Name
-											</label>
-										</div>
-										<div
-											className="relative mb-6 border border-light4 rounded-lg"
-											data-te-input-wrapper-init>
-											<input
-												type="email"
-												className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-												id="exampleInput91"
-												placeholder="Email address"
-											/>
-											<label
-												className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-sm peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  dark:peer-focus:text-sm"
-												for="exampleInput91">
-												Email address
-											</label>
-										</div>
-										<div
-											className="relative mb-6 border border-light4 rounded-lg"
-											data-te-input-wrapper-init>
-											<textarea
-												className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-												id="exampleFormControlTextarea1"
-												rows="3"
-												placeholder="Your message"></textarea>
-											<label
-												for="exampleFormControlTextarea1"
-												className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  dark:peer-focus:text-primary">
-												Message
-											</label>
-										</div>
-										<div className="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
-											<input
-												className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent  dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-												type="checkbox"
-												value=""
-												id="exampleCheck96"
-												checked
-											/>
-											<label
-												className="inline-block pl-[0.15rem] hover:cursor-pointer text-xs"
-												for="exampleCheck96">
-												Send me a copy of this
-												message
-											</label>
-										</div>
-										<button
-											type="button"
-											data-te-ripple-init
-											data-te-ripple-color="light"
-											className="mb-6 inline-block w-full rounded bg-dark2 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-lg focus:bg-dark1 focus:outline-none focus:ring-0  lg:mb-0">
-											Send
-										</button>
-									</form>
+								<div className="w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
+									<Player
+										autoplay
+										loop
+										src="https://assets9.lottiefiles.com/packages/lf20_v7gj8hb1.json"
+										style={{
+											height: '300px',
+										}}></Player>
 								</div>
+								{/* Team Info */}
 								<div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
 									<div className="flex flex-wrap">
 										<div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
