be8a9d270488668bf400a9aefd5e9df56ea803db
Daurius Swarts <ali@openmindi.co.za>
2023-06-10 18:42:37 +0200

    [STYLE] About update

--- frontend/src/page/AboutUs.js
+++ frontend/src/page/AboutUs.js
@@ -169,7 +169,7 @@ const AboutUs = () => {
 									</div>
 								</div>
 
-								<div className="mb-12 w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:px-3">
+								<div className="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:px-3">
 									<div className="flex">
 										<div className="shrink-0">
 											<div className="mb-6 inline-block rounded-md bg-primary-100 p-4 text-primary">
@@ -267,76 +267,123 @@ const AboutUs = () => {
 					<div className="container px-6 md:px-12">
 						<div className="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
 							{/* Team Info */}
-							<div className="">
-								<ul className="grid gap-x-4 gap-y-4 sm:grid-cols-3 xl:col-span-3">
-									{People.map(person => (
-										<li key={person.name}>
-											<div
-												className="flex items-center gap-x-2 bg-[#fafafa] p-5 rounded-md shadow-2xl border border-white hover:border-none
-                  										 hover:bg-[#f4f4f4]">
-												<img
-													className="h-16 w-16 rounded-full"
-													src={person.imageUrl}
-													alt=""
-												/>
-												<div>
-													<h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-														{person.name}
-													</h3>
-													<p className="text-xs font-medium text-gray-400">
-														{person.role}
-													</p>
-													<ul className="mt-2 flex gap-x-3">
-														<li>
-															<a
-																href={
-																	person.githubUrl
-																}
-																className="text-gray-400 hover:text-gray-500"
-																target="_blank"
-																rel="noreferrer">
-																<span className="sr-only">
-																	GitHub
-																</span>
-																<svg
-																	className="h-4 w-4"
-																	aria-hidden="true"
-																	fill="currentColor"
-																	viewBox="0 0 25 25">
-																	<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-																</svg>
-															</a>
-														</li>
-														<li>
-															<a
-																href={
-																	person.linkedinUrl
-																}
-																className="text-gray-400 hover:text-gray-500"
-																target="_blank"
-																rel="noreferrer">
-																<span className="sr-only">
-																	LinkedIn
-																</span>
-																<svg
-																	className="h-4 w-4"
-																	aria-hidden="true"
-																	fill="currentColor"
-																	viewBox="0 0 20 20">
-																	<path
-																		fillRule="evenodd"
-																		d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-																		clipRule="evenodd"
-																	/>
-																</svg>
-															</a>
-														</li>
-													</ul>
+							<div class="container mx-auto md:px-6">
+								<section class="mb-32">
+									<div class="flex justify-center">
+										<div class="text-center md:max-w-xl lg:max-w-3xl">
+											<h2 class="mb-12 px-6 text-3xl font-bold">
+												Contact us
+											</h2>
+										</div>
+									</div>
+
+									<div class="flex flex-wrap">
+										<div class="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
+											<Player
+												autoplay
+												loop
+												src="https://assets2.lottiefiles.com/packages/lf20_v7gj8hb1.json"
+												style={{
+													height: '300px',
+												}}></Player>
+										</div>
+										<div class="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
+											<div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6">
+												<div class="w-full md:px-3 lg:px-6">
+													<div class="grid gap-x-4 gap-y-4 sm:grid-cols-3 xl:col-span-3">
+														{People.map(
+															person => (
+																<div
+																	key={
+																		person.name
+																	}
+																	className="ml-6 grow">
+																	<h4 className="mb-1 font-medium text-base text-dark2">
+																		{person.name}
+																	</h4>
+																	<div className="text-dark5 text-xs">
+																		<p>
+																			{
+																				person.role
+																			}
+																		</p>
+																		<p>
+																			{
+																				person.email
+																			}
+																		</p>
+																		<ul className="mt-2 flex gap-x-3">
+																			<li>
+																				<a
+																					href={
+																						person.githubUrl
+																					}
+																					className="text-gray-400 hover:text-gray-500"
+																					target="_blank"
+																					rel="noreferrer">
+																					<span className="sr-only">
+																						GitHub
+																					</span>
+																					<svg
+																						className="h-4 w-4"
+																						aria-hidden="true"
+																						fill="currentColor"
+																						viewBox="0 0 25 25">
+																						<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+																					</svg>
+																				</a>
+																			</li>
+																			<li>
+																				<a
+																					href={
+																						person.linkedinUrl
+																					}
+																					className="text-gray-400 hover:text-gray-500"
+																					target="_blank"
+																					rel="noreferrer">
+																					<span className="sr-only">
+																						LinkedIn
+																					</span>
+																					<svg
+																						className="h-4 w-4"
+																						aria-hidden="true"
+																						fill="currentColor"
+																						viewBox="0 0 20 20">
+																						<path
+																							fillRule="evenodd"
+																							d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+																							clipRule="evenodd"
+																						/>
+																					</svg>
+																				</a>
+																			</li>
+																		</ul>
+																	</div>
+																</div>
+															)
+														)}
+													</div>
+												</div>
+
+												<div class="w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+													<div class="align-start flex">
+														<div class="ml-6 grow text-dark5 text-xs">
+															<h3 class="mb-1 font-medium text-base text-dark2">
+																GitHub Repo
+															</h3>
+															<p class="text-dark5">
+																bugs@example.com
+															</p>
+															<p class="text-dark5">
+																+1 234-567-89
+															</p>
+														</div>
+													</div>
 												</div>
 											</div>
-										</li>
-									))}
-								</ul>
+										</div>
+									</div>
+								</section>
 							</div>
 						</div>
 					</div>
