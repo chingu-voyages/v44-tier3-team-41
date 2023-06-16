40dd349161fcd8e2d5f8927c661cd16c76d28829
Daurius Swarts <ali@openmindi.co.za>
2023-06-16 10:57:31 +0200

    [STYLE] Updated our contacts

--- frontend/src/page/AboutUs.js
+++ frontend/src/page/AboutUs.js
@@ -1,7 +1,7 @@
-import { Player } from '@lottiefiles/react-lottie-player';
+import {Player} from '@lottiefiles/react-lottie-player';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 import People from '../assets/People.json';
 
 const AboutUs = () => {
@@ -271,7 +271,7 @@ const AboutUs = () => {
 								<section className="mb-8">
 									<div className="flex justify-center">
 										<div className="text-center md:max-w-xl lg:max-w-3xl">
-											<h2 className="mb-6 px-6 text-3xl font-bold">
+											<h2 className="px-6 text-xl font-bold">
 												Contact us
 											</h2>
 										</div>
@@ -284,56 +284,63 @@ const AboutUs = () => {
 												loop
 												src="https://assets2.lottiefiles.com/packages/lf20_v7gj8hb1.json"
 												style={{
-													height: '400px',
-												}}
-											></Player>
+													height: '300px',
+												}}></Player>
 										</div>
-										<div className="w-full md:w-10/12">
-											<div className="mx-auto grid gap-x-4 gap-y-20 px-2">
-												<div className="grid grid-cols-2 lg:grid-cols-3 ">
-													{People.map((person) => (
-														<div key={person.name} className="ml-2 grow mt-6">
-															<h4 className="mb-1 font-medium text-lg text-dark2 whitespace-nowrap">
+										<div className="w-full -mt-10">
+											<div className="mx-auto grid gap-x-4 px-2 text-center">
+												<div className="grid grid-cols-1 lg:grid-cols-3">
+													{People.map(person => (
+														<div
+															key={person.name}
+															className="ml-2 grow mt-6 bg-light1 border p-5 rounded-lg">
+															<h4 className="mb-1 font-medium text-base text-dark2 whitespace-nowrap">
 																{person.name}
 															</h4>
-															<div className="text-dark5 text-sm">
-																<p>{person.role}</p>
-																<p>{person.email}</p>
-																<ul className="mt-2 flex gap-x-3">
-																	<li>
+															<div className="text-dark5 text-xs">
+																<p>
+																	{person.role}
+																</p>
+																<p>
+																	{person.email}
+																</p>
+																<ul className="mt-2 flex gap-x-3 items-center justify-center">
+																	<li className="flex">
 																		<a
-																			href={person.githubUrl}
+																			href={
+																				person.githubUrl
+																			}
 																			className="text-gray-400 hover:text-gray-500"
 																			target="_blank"
-																			rel="noreferrer"
-																		>
-																			<span className="sr-only">GitHub</span>
+																			rel="noreferrer">
+																			<span className="sr-only">
+																				GitHub
+																			</span>
 																			<svg
-																				className="h-6 w-6"
+																				className="h-4 w-4"
 																				aria-hidden="true"
 																				fill="currentColor"
-																				viewBox="0 0 25 25"
-																			>
-																				<path
-																					d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-																				/>
+																				viewBox="0 0 25 25">
+																				<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
 																			</svg>
 																		</a>
 																	</li>
 																	<li>
 																		<a
-																			href={person.linkedinUrl}
+																			href={
+																				person.linkedinUrl
+																			}
 																			className="text-gray-400 hover:text-gray-500"
 																			target="_blank"
-																			rel="noreferrer"
-																		>
-																			<span className="sr-only">LinkedIn</span>
+																			rel="noreferrer">
+																			<span className="sr-only">
+																				LinkedIn
+																			</span>
 																			<svg
-																				className="h-6 w-6"
+																				className="h-4 w-4"
 																				aria-hidden="true"
 																				fill="currentColor"
-																				viewBox="0 0 20 20"
-																			>
+																				viewBox="0 0 20 20">
 																				<path
 																					fillRule="evenodd"
 																					d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
@@ -356,7 +363,6 @@ const AboutUs = () => {
 					</div>
 				</section>
 			</div>
-
 		</div>
 	);
 };
